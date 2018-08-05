05163a4fed877b4f8a8da40e1a4dbdfbdb34a1d6
alexmochu <mochualex4@gmail.com>
2018-08-05 14:56:25 +0300

    Setup Redux

--- src/components/Postform.js
+++ src/components/Postform.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react'
-
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { createPost } from '../actions/postActions'
 class Postform extends Component {
     constructor(props){
         super(props);
@@ -22,17 +24,10 @@ class Postform extends Component {
         const post = {
             title: this.state.title,
             body: this.state.body
-        }
+        };
 
-        fetch('https://jsonplaceholder.typicode.com/posts',{
-            method: 'POST',
-            headers: {
-                'content-type': 'application/json'
-            },
-            body: JSON.stringify(post)
-        })
-        .then(res => res.json())
-        .then(data => console.log(data));
+        this.props.createPost(post);
+
     }
 
   render() {
@@ -58,4 +53,8 @@ class Postform extends Component {
   }
 }
 
-export default Postform
+Postform.propTypes = {
+    createPost: PropTypes.func.isRequired
+};
+
+export default connect(null, { createPost })(Postform)
